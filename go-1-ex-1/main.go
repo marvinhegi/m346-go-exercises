44e6f9311c3bea7a2743e1d89ea92006218f7e5b
marvinhegi <153171203+marvinhegi@users.noreply.github.com>
2024-11-22 09:30:56 +0100

    Update main.go Aufgabe1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,7 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: Declare and initialize the variables being used in the output!
+	var firstName string = "Marvin"
+	var lastName string = "Hegi"
+
+	var dayOfBirth int = 24
+	var monthOfBirth int = 12
+	var yearOfBirth int = 2007
+
+	var numberOfSiblings int = 1
+
+	var heightInMeters float32 = 1.80
+
+	var zodiacSign string = "Steinbock"
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
