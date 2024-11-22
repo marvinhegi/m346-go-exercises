2d987dda5acc5a83cabcda97576b6f4816fdc132
marvinhegi <153171203+marvinhegi@users.noreply.github.com>
2024-11-22 09:54:15 +0100

    Update main.go

--- go-1-ex-3/main.go
+++ go-1-ex-3/main.go
@@ -3,19 +3,18 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
 func main() {
-	var eyes = rand.Intn(5) + 1
+	var eyes = rand.Intn(5) + 1 
 	var when = time.Now()
 
-	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice shows", eyes, "eyes")
+	fmt.Fprintln(os.Stdout, "the dice shows", eyes, "eyes")
 
-	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice was rolled at", when)
+	fmt.Fprintln(os.Stderr, "the dice was rolled at", when)
 
-	// TODO: how to write the output into eyes.txt and dice.log?
-	// go run ex3/main.go TODO
-}
+	//Aufruf über Git Bash
+	//go run main.go > eyes.txt 2> dice.log
+} 
