9f3e9d89ea8e6c40eb8a43bc1a4c6c4a2d6f0e85
marvinhegi <153171203+marvinhegi@users.noreply.github.com>
2024-11-22 09:37:22 +0100

    Update main.go

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -5,16 +5,13 @@ import "fmt"
 func main() {
 	var firstName string = "Marvin"
 	var lastName string = "Hegi"
-
 	var dayOfBirth int = 24
 	var monthOfBirth int = 12
 	var yearOfBirth int = 2007
-
 	var numberOfSiblings int = 1
-
 	var heightInMeters float32 = 1.80
-
-	var zodiacSign string = "Steinbock"
+	var zodiacSign rune = '\u2651'
+	
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
