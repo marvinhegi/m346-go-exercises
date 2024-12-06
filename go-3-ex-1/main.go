5b81c773f153952d4f271bcb9d3af7e082c0da92
marvinhegi <153171203+marvinhegi@users.noreply.github.com>
2024-12-06 13:02:41 +0100

    Update main.go

--- go-3-ex-1/main.go
+++ go-3-ex-1/main.go
@@ -20,16 +20,16 @@ const (
 func outputWithZodiacSign(p Person) {
 	var zodiacSign rune = '?'
 
-	if (p.Month == 3 && p.Day >= 21) || (p.Month == 4 && p.Day <= 19) {
+	if (p.Month == 3 && p.Day >= 21) || (p.Month == 4 && p.Day <= 20) {
 		zodiacSign = Aries
 		
-	} else if (p.Month == 4 && p.Day >= 20) || (p.Month == 5 && p.Day <= 20) {
+	} else if (p.Month == 4 && p.Day >= 21) || (p.Month == 5 && p.Day <= 21) {
 		zodiacSign = Taurus
 		
-	} else if (p.Month == 5 && p.Day >= 21) || (p.Month == 6 && p.Day <= 20) {
+	} else if (p.Month == 5 && p.Day >= 22) || (p.Month == 6 && p.Day <= 21) {
 		zodiacSign = Gemini
 		
-	} else if (p.Month == 6 && p.Day >= 21) || (p.Month == 7 && p.Day <= 22) {
+	} else if (p.Month == 6 && p.Day >= 22) || (p.Month == 7 && p.Day <= 22) {
 		zodiacSign = Cancer
 		
 	} else if (p.Month == 7 && p.Day >= 23) || (p.Month == 8 && p.Day <= 22) {
@@ -41,13 +41,13 @@ func outputWithZodiacSign(p Person) {
 	} else if (p.Month == 9 && p.Day >= 23) || (p.Month == 10 && p.Day <= 22) {
 		zodiacSign = Libra
 		
-	} else if (p.Month == 10 && p.Day >= 23) || (p.Month == 11 && p.Day <= 21) {
+	} else if (p.Month == 10 && p.Day >= 23) || (p.Month == 11 && p.Day <= 22) {
 		zodiacSign = Scorpius
 		
-	} else if (p.Month == 11 && p.Day >= 22) || (p.Month == 12 && p.Day <= 21) {
+	} else if (p.Month == 11 && p.Day >= 23) || (p.Month == 12 && p.Day <= 20) {
 		zodiacSign = Sagittarius
 		
-	} else if (p.Month == 12 && p.Day >= 22) || (p.Month == 1 && p.Day <= 19) {
+	} else if (p.Month == 12 && p.Day >= 21) || (p.Month == 1 && p.Day <= 19) {
 		zodiacSign = Capricornus
 		
 	} else if (p.Month == 1 && p.Day >= 20) || (p.Month == 2 && p.Day <= 18) {
@@ -85,6 +85,16 @@ func main() {
 
 	outputWithZodiacSign(grace)
 	outputWithZodiacSign(dennis)
+	outputWithZodiacSign(rick)
+	outputWithZodiacSign(edsger)
+	outputWithZodiacSign(alan)
+
+
+	/* Zusatzaufgabe */
+	rick := Person{FullName{"Marvin", "Hegi"}, BirthDate{24, 12, 2007}}
+	edsger := Person{FullName{"Mascha", "Hegi"}, BirthDate{5, 8, 2010}}
+	alan := Person{FullName{"Nicole", "Hegi"}, BirthDate(9, 1, 1976}}
+
 	outputWithZodiacSign(rick)
 	outputWithZodiacSign(edsger)
 	outputWithZodiacSign(alan)
