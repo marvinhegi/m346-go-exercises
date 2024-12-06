4d4c21d14c94566d63f050f3843ac18c8deecdf2
marvinhegi <153171203+marvinhegi@users.noreply.github.com>
2024-12-06 13:16:46 +0100

    Update main.go

--- go-3-ex-3/main.go
+++ go-3-ex-3/main.go
@@ -1,4 +1,5 @@
 package main
+import "fmt"
 
 const (
 	Lower = 1
@@ -6,5 +7,21 @@ const (
 )
 
 func main() {
-	// TODO: Implement FizzBuzz using a for loop from Lower to Upper.
+	
+for i := Lower; i <= Upper; i++ {
+	switch {
+	case i%3 == 0 && i%5 == 0:
+		fmt.Println("FizzBuzz")
+		
+	case i%3 == 0:
+		fmt.Println("Fizz")
+		
+	case i%5 == 0:
+		fmt.Println("Buzz")
+		
+	default:
+		fmt.Println(i)
+	}
+}
+	
 }
