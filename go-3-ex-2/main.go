e15ea1be5f20c6a11e16c42bbfcb973867879f48
marvinhegi <153171203+marvinhegi@users.noreply.github.com>
2024-12-06 13:13:00 +0100

    Update main.go

--- go-3-ex-2/main.go
+++ go-3-ex-2/main.go
@@ -19,16 +19,34 @@ const (
 
 func outputDateRange(zodiacSign rune) {
 	fmt.Printf("%c: ", zodiacSign)
-	// TODO: Replace if, else if branching with switch/case.
-	// TODO: Define all 12 cases...
-	if zodiacSign == Aries {
-		fmt.Println("21.03. - 20.04")
-	} else if zodiacSign == Taurus {
-		fmt.Println("21.04. - 21.05")
-	} else {
-		fmt.Println("")
+	switch zodiacSign {
+	case Aries:
+		fmt.Println("21.03. - 20.04.")
+	case Taurus:
+		fmt.Println("21.04. - 21.05.")
+	case Gemini:
+		fmt.Println("22.05. - 21.06.")
+	case Cancer:
+		fmt.Println("22.06. - 22.07.")
+	case Leo:
+		fmt.Println("23.07. - 22.08.")
+	case Virgo:
+		fmt.Println("23.08. - 22.09.")
+	case Libra:
+		fmt.Println("23.09. - 22.10.")
+	case Scorpius:
+		fmt.Println("23.10. - 22.11.")
+	case Sagittarius:
+		fmt.Println("23.11. - 20.12.")
+	case Capricornus:
+		fmt.Println("21.12. - 19.01.")
+	case Aquarius:
+		fmt.Println("20.01. - 18.02.")
+	case Pisces:
+		fmt.Println("29.02. - 20.03.")
+	default:
+		fmt.Println("Unknown zodiac sign.")
 	}
-	// TODO: ...and consider a default case.
 }
 
 func main() {
