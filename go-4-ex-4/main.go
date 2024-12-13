4076c4c5abe0c3c66530beb454fe2ba29d3b960e
marvinhegi <153171203+marvinhegi@users.noreply.github.com>
2024-12-13 13:01:37 +0100

    Update main.go

--- go-4-ex-4/main.go
+++ go-4-ex-4/main.go
@@ -1,9 +1,31 @@
 package main
 
-// TODO: implement the function convertCelsiusToFahrenheit
-// TODO: implement the function convertFahrenheitToCelsius
+import "fmt"
+
+func convertCelsiusToFahrenheit(c float64) float64 {
+    
+	return c*9/5 + 32
+}
+
+func convertFahrenheitToCelsius(f float64) float64 {
+    
+	return (f - 32) * 5 / 9
+}
 
 func main() {
-	// TODO: call the function convertCelsiusToFahrenheit
-	// TODO: call the function convertFahrenheitToCelsius
+	c1, c2, c3 := 0.0, 50.0, 100.0
+
+	f1 := convertCelsiusToFahrenheit(c1)
+	f2 := convertCelsiusToFahrenheit(c2)
+	f3 := convertCelsiusToFahrenheit(c3)
+
+	fmt.Println("Celsius to Fahrenheit:")
+	fmt.Println(c1, "°C ->", f1, "°F") //32
+	fmt.Println(c2, "°C ->", f2, "°F") //122
+	fmt.Println(c3, "°C ->", f3, "°F") //212
+
+	fmt.Println("Fahrenheit back to Celsius:")
+	fmt.Println(f1, "°F ->", convertFahrenheitToCelsius(f1), "°C")
+	fmt.Println(f2, "°F ->", convertFahrenheitToCelsius(f2), "°C")
+	fmt.Println(f3, "°F ->", convertFahrenheitToCelsius(f3), "°C")
 }
