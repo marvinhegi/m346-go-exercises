eb79fdd7a6a2b2aeada42e90f31fe7d04feec466
marvinhegi <153171203+marvinhegi@users.noreply.github.com>
2024-12-13 12:50:39 +0100

    Update main.go

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,21 @@
 package main
 
-// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+import (
+	"fmt"
+	"math"
+)
+
+func computeHypotenuse(sideA float64, sideB float64) float64 {
+    
+	c := math.Sqrt(math.Pow(sideA, 2) + math.Pow(sideB, 2))
+	return c
+	
+}
 
 func main() {
-	// TODO: call the function computeHypotenuse
+    
+	fmt.Println(computeHypotenuse(30, 40))  //50
+	fmt.Println(computeHypotenuse(20, 30))  //36.05551275463989
+	fmt.Println(computeHypotenuse(10, 10))  //14.142135623730951
+
 }
