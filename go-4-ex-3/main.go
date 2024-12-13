6c69eeb0f5d9070aca74b00e2a2cb5f0bdcd7d59
marvinhegi <153171203+marvinhegi@users.noreply.github.com>
2024-12-13 12:55:46 +0100

    Update main.go

--- go-4-ex-3/main.go
+++ go-4-ex-3/main.go
@@ -1,7 +1,32 @@
 package main
 
-// TODO: implement the function computeQuadraticFormula
+import (
+	"fmt"
+	"math"
+)
+
+func computeQuadraticFormula(a, b, c float64) []float64 {
+    
+	D := math.Pow(b, 2) - 4*a*c
+	
+	if D > 0 {
+	    
+		return []float64{
+			(-b + math.Sqrt(D)) / (2 * a),
+			(-b - math.Sqrt(D)) / (2 * a),
+		}
+		
+	} else if D == 0 {
+	    
+		return []float64{-b / (2 * a)}
+		
+	}
+	
+	return []float64{}
+}
 
 func main() {
-	// TODO: call the function computeQuadraticFormula
+	fmt.Println(computeQuadraticFormula(3, 4, 1)) //resultat -0.3 und -1
+	fmt.Println(computeQuadraticFormula(2, 4, 2)) // resultat -1
+	fmt.Println(computeQuadraticFormula(3, 4, 2)) //kein resultat
 }
